5b22e708569062cc0b48a9ed347b201d7d92aa3a
1GodLess5 <gacekalbert@seznam.cz>
2024-10-14 18:33:21 +0200

    Added enum states, new method fromString()

--- src/main/java/cz/godless/task_management_system/domain/TaskStatus.java
+++ src/main/java/cz/godless/task_management_system/domain/TaskStatus.java
@@ -1,2 +1,14 @@
-package cz.godless.task_management_system.domain;public enum TaskStatus {
+package cz.godless.task_management_system.domain;
+
+public enum TaskStatus {
+    NEW,
+    DONE;
+
+    public static TaskStatus fromString(String status) {
+        return switch (status) {
+            case "NEW" -> NEW;
+            case "DONE" -> DONE;
+            default -> throw new IllegalArgumentException("Unknown task status: " + status);
+        };
+    }
 }
