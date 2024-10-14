6bad885738e8024e1ab7b3cabe87b66766bb4a1b
1GodLess5 <gacekalbert@seznam.cz>
2024-10-14 19:32:23 +0200

    Added properties, getters, setters, no args constructor, all args constructor

--- src/main/java/cz/godless/task_management_system/api/request/UserAddRequest.java
+++ src/main/java/cz/godless/task_management_system/api/request/UserAddRequest.java
@@ -1,2 +1,13 @@
-package cz.godless.task_management_system.api.request;public class UserAddRequest {
+package cz.godless.task_management_system.api.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserAddRequest {
+    private String name;
+    private String email;
 }
