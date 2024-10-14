4d9a6df1612c41da05091099fe776734df0665bf
1GodLess5 <gacekalbert@seznam.cz>
2024-10-14 18:28:36 +0200

    Added properties, constructor, getters

--- src/main/java/cz/godless/task_management_system/domain/Project.java
+++ src/main/java/cz/godless/task_management_system/domain/Project.java
@@ -1,2 +1,32 @@
-package cz.godless.task_management_system.domain;public class Project {
+package cz.godless.task_management_system.domain;
+
+public class Project {
+    private final long id;
+    private final long userId;
+    private final String name;
+    private final String description;
+
+
+    public Project(long id, long userId, String name, String description) {
+        this.id = id;
+        this.userId = userId;
+        this.name = name;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
