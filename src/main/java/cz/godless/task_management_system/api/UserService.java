cefc00516e3a5e36d01a41437f3dd543d8e3a18b
1GodLess5 <gacekalbert@seznam.cz>
2024-10-14 19:34:26 +0200

    defined methods add(), delete(), get(), getAll()

--- src/main/java/cz/godless/task_management_system/api/UserService.java
+++ src/main/java/cz/godless/task_management_system/api/UserService.java
@@ -1,2 +1,13 @@
-package cz.godless.task_management_system.api;public interface UserService {
+package cz.godless.task_management_system.api;
+
+import cz.godless.task_management_system.api.request.UserAddRequest;
+import cz.godless.task_management_system.domain.User;
+
+import java.util.List;
+
+public interface UserService {
+    long add(UserAddRequest request);
+    void delete(long id);
+    User get(long id);
+    List<User> getAll();
 }
