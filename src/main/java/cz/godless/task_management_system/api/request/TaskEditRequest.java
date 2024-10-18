f9d4277f17d64fc84887903847121d60d269bd93
1GodLess5 <gacekalbert@seznam.cz>
2024-10-18 11:47:45 +0200

    Added properties, @Data, @NoArgs, @AllArgs

--- src/main/java/cz/godless/task_management_system/api/request/TaskEditRequest.java
+++ src/main/java/cz/godless/task_management_system/api/request/TaskEditRequest.java
@@ -1,5 +1,6 @@
 package cz.godless.task_management_system.api.request;
 
+import cz.godless.task_management_system.domain.TaskStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,9 +8,8 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class TaskAddRequest {
-    private long userId;
-    private Long projectId;
+public class TaskEditRequest {
     private String name;
     private String description;
+    private TaskStatus status;
 }
