5b7319895e358421713c67d5c735af0f8a9cf731
1GodLess5 <gacekalbert@seznam.cz>
2024-10-19 00:57:02 +0200

    Added mapRow User -> map one row of table to domain/user

--- src/main/java/cz/godless/task_management_system/implementation/jdbc/mapper/UserRowMapper.java
+++ src/main/java/cz/godless/task_management_system/implementation/jdbc/mapper/UserRowMapper.java
@@ -1,4 +1,19 @@
 package cz.godless.task_management_system.implementation.jdbc.mapper;
 
-public class UserRowMapper {
+import cz.godless.task_management_system.domain.User;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class UserRowMapper implements RowMapper<User> {
+    @Override
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new User(
+                rs.getInt("id"),
+                rs.getString("name"),
+                rs.getString("email"));
+    }
 }
