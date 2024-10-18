016da0b7e3ad5adf6d7627b033590ea31afd37ca
1GodLess5 <gacekalbert@seznam.cz>
2024-10-18 11:41:45 +0200

    Added methods to manage domain/Project

--- src/main/java/cz/godless/task_management_system/api/ProjectService.java
+++ src/main/java/cz/godless/task_management_system/api/ProjectService.java
@@ -1,4 +1,17 @@
 package cz.godless.task_management_system.api;
 
+import cz.godless.task_management_system.api.request.ProjectAddRequest;
+import cz.godless.task_management_system.api.request.ProjectEditRequest;
+import cz.godless.task_management_system.domain.Project;
+
+import java.util.List;
+
 public interface ProjectService {
+    Project get(long id);
+    List<Project> getAll();
+    List<Project> getAllByUser(long userId);
+    void delete(long id);
+
+    long add (ProjectAddRequest request);
+    void edit(long id, ProjectEditRequest request);
 }
