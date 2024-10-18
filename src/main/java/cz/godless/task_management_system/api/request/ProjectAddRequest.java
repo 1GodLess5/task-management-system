14fff0b5fcac57eca52d94221dc1519ef6d75283
1GodLess5 <gacekalbert@seznam.cz>
2024-10-18 11:37:19 +0200

    Added properties, @Data, @NoArgs, @AllArgs

--- src/main/java/cz/godless/task_management_system/api/request/ProjectAddRequest.java
+++ src/main/java/cz/godless/task_management_system/api/request/ProjectAddRequest.java
@@ -1,4 +1,14 @@
 package cz.godless.task_management_system.api.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProjectAddRequest {
+    private Long userId;
+    private String name;
+    private String description;
 }
