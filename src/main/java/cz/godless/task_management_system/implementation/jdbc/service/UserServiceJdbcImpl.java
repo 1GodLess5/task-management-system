6d1de7fb974714a437e6abc725edfe8765f2c542
1GodLess5 <gacekalbert@seznam.cz>
2024-10-21 06:24:30 +0200

    Refactor: UserJdbcRepository to repository, @Override method get() - get by ID

--- src/main/java/cz/godless/task_management_system/implementation/jdbc/service/UserServiceJdbcImpl.java
+++ src/main/java/cz/godless/task_management_system/implementation/jdbc/service/UserServiceJdbcImpl.java
@@ -10,10 +10,10 @@ import java.util.List;
 
 @Service
 public class UserServiceJdbcImpl implements UserService {
-    private final UserJdbcRepository userJdbcRepository;
+    private final UserJdbcRepository repository;
 
     public UserServiceJdbcImpl(UserJdbcRepository userJdbcRepository) {
-        this.userJdbcRepository = userJdbcRepository;
+        this.repository = userJdbcRepository;
     }
 
     @Override
@@ -28,11 +28,11 @@ public class UserServiceJdbcImpl implements UserService {
 
     @Override
     public User get(long id) {
-        return null;
+        return repository.getById(id);
     }
 
     @Override
     public List<User> getAll() {
-        return userJdbcRepository.getAll();
+        return repository.getAll();
     }
 }
