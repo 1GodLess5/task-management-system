d2e77f7d882dbb19dd2b82d9d0d65646bc5a5db5
1GodLess5 <gacekalbert@seznam.cz>
2024-10-21 06:31:14 +0200

    New @GetMapping: implemented getById()

--- src/main/java/cz/godless/task_management_system/controller/UserController.java
+++ src/main/java/cz/godless/task_management_system/controller/UserController.java
@@ -4,6 +4,7 @@ import cz.godless.task_management_system.api.UserService;
 import cz.godless.task_management_system.domain.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,10 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<User>>  getAll() {
         return ResponseEntity.ok().body(userService.getAll());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<User> getById(@PathVariable("id") long id) {
+        return ResponseEntity.ok().body(userService.get(id));
     }
 }
