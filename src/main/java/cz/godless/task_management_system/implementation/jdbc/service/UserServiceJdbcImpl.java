c042bfe47f7ab861f11455998588c066b30e8db7
1GodLess5 <gacekalbert@seznam.cz>
2024-10-21 06:15:05 +0200

    Implemented UserService, @Override method getAll()

--- src/main/java/cz/godless/task_management_system/implementation/jdbc/service/UserServiceJdbcImpl.java
+++ src/main/java/cz/godless/task_management_system/implementation/jdbc/service/UserServiceJdbcImpl.java
@@ -1,4 +1,38 @@
 package cz.godless.task_management_system.implementation.jdbc.service;
 
-public class UserServiceJdbcImpl {
+import cz.godless.task_management_system.api.UserService;
+import cz.godless.task_management_system.api.request.UserAddRequest;
+import cz.godless.task_management_system.domain.User;
+import cz.godless.task_management_system.implementation.jdbc.repository.UserJdbcRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceJdbcImpl implements UserService {
+    private final UserJdbcRepository userJdbcRepository;
+
+    public UserServiceJdbcImpl(UserJdbcRepository userJdbcRepository) {
+        this.userJdbcRepository = userJdbcRepository;
+    }
+
+    @Override
+    public long add(UserAddRequest request) {
+        return 0;
+    }
+
+    @Override
+    public void delete(long id) {
+
+    }
+
+    @Override
+    public User get(long id) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return userJdbcRepository.getAll();
+    }
 }
