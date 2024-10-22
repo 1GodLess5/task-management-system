ab8f8a872cdbf2f6b2308e914467365d58154758
1GodLess5 <gacekalbert@seznam.cz>
2024-10-22 08:18:31 +0200

    Integration Test setup

--- src/test/java/cz/godless/task_management_system/IntergrationTest.java
+++ src/test/java/cz/godless/task_management_system/IntergrationTest.java
@@ -1,4 +1,13 @@
 package cz.godless.task_management_system;
 
-public class IntergrationTest {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public abstract class IntergrationTest {
+    @Autowired
+    protected TestRestTemplate restTemplate;
 }
