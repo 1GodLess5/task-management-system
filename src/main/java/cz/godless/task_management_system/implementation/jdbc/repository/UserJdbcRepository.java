97677a4d54f55b70b8e6c3a0d29c7a11433b4182
1GodLess5 <gacekalbert@seznam.cz>
2024-10-22 07:58:03 +0200

    New method delete()

--- src/main/java/cz/godless/task_management_system/implementation/jdbc/repository/UserJdbcRepository.java
+++ src/main/java/cz/godless/task_management_system/implementation/jdbc/repository/UserJdbcRepository.java
@@ -28,12 +28,14 @@ public class UserJdbcRepository {
     private static final String GET_ALL;
     private static final String GET_BY_ID;
     private static final String INSERT;
+    private static final String DELETE;
 
     static {
         logger = LoggerFactory.getLogger(UserJdbcRepository.class);
         GET_ALL = "SELECT * FROM user";
         GET_BY_ID = "SELECT * FROM user WHERE id = ?";
         INSERT = "INSERT INTO user(id, name, email) VALUES (next value for user_id_seq, ?, ?)";
+        DELETE = "DELETE FROM user WHERE id = ?";
     }
 
     public UserJdbcRepository(JdbcTemplate jdbcTemplate, UserRowMapper userRowMapper) {
@@ -64,6 +66,15 @@ public class UserJdbcRepository {
             throw new InternalErrorException("Error while adding user");
         }
 
+    }
+
+    public void delete(long id) {
+        try {
+            jdbcTemplate.update(DELETE, id);
+        } catch (DataAccessException e) {
+            logger.error("Error while deleting user", e);
+            throw new InternalErrorException("Error while deleting user");
+        }
     }
 
     public List<User> getAll() {
