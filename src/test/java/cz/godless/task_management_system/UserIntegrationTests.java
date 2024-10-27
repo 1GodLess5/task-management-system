363e2170dfe0fc95ca785b585c76825c58f2e039
1GodLess5 <gacekalbert@seznam.cz>
2024-10-27 18:18:40 +0100

    + test methods: getAll(), insert() + help methods: insertUser(), generateRandomUser()

--- src/test/java/cz/godless/task_management_system/UserIntegrationTests.java
+++ src/test/java/cz/godless/task_management_system/UserIntegrationTests.java
@@ -1,4 +1,53 @@
 package cz.godless.task_management_system;
 
-public class UserIntegrationTests {
+import cz.godless.task_management_system.api.request.UserAddRequest;
+import cz.godless.task_management_system.domain.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public class UserIntegrationTests extends IntergrationTest {
+    @Test
+    public void getAll() {
+        final ResponseEntity<List<User>> userResponse =  restTemplate.exchange(
+                "/user",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {}
+        );
+
+        Assertions.assertEquals(HttpStatus.OK, userResponse.getStatusCode());
+        Assertions.assertNotNull(userResponse.getBody());
+        Assertions.assertTrue(userResponse.getBody().size() >= 2);
+    }
+
+    @Test
+    public void insert() {
+        insertUser(generateRandomUser());
+    }
+
+    private UserAddRequest generateRandomUser() {
+        return new UserAddRequest(
+                "name" + Math.random(),
+                "email" + Math.random()
+        );
+    }
+
+    private long insertUser(UserAddRequest request) {
+        final ResponseEntity<Long> insertResponse = restTemplate.postForEntity(
+                "/user",
+                request,
+                Long.class
+        );
+
+        Assertions.assertEquals(HttpStatus.CREATED, insertResponse.getStatusCode());
+        Assertions.assertNotNull(insertResponse.getBody());
+
+        return insertResponse.getBody();
+    }
 }
