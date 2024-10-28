b4aa76a41e80996e0d142a42c79c842378b14093
1GodLess5 <gacekalbert@seznam.cz>
2024-10-28 22:00:40 +0100

    + method: getById()

--- src/main/java/cz/godless/task_management_system/implementation/jdbc/repository/ProjectJdbcRepository.java
+++ src/main/java/cz/godless/task_management_system/implementation/jdbc/repository/ProjectJdbcRepository.java
@@ -2,11 +2,13 @@ package cz.godless.task_management_system.implementation.jdbc.repository;
 
 import cz.godless.task_management_system.api.ProjectService;
 import cz.godless.task_management_system.api.exception.InternalErrorException;
+import cz.godless.task_management_system.api.exception.ResourceNotFoundException;
 import cz.godless.task_management_system.domain.Project;
 import cz.godless.task_management_system.implementation.jdbc.mapper.ProjectRowMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -40,6 +42,17 @@ public class ProjectJdbcRepository {
         } catch (DataAccessException e) {
             logger.error("Error while getting all projects", e);
             throw new InternalErrorException("Error while getting all projects");
+        }
+    }
+
+    public Project getById(long id) {
+        try {
+            return jdbcTemplate.queryForObject(GET_BY_ID, rowMapper, id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Project with ID " + id + " was not found");
+        } catch (DataAccessException e) {
+            logger.error("Error while getting project with ID " + id, e);
+            throw new InternalErrorException("Error while getting project with ID " + id);
         }
     }
 }
