3db1dc10ccf464547c8a86dd5bb2c710fec8c946
1GodLess5 <gacekalbert@seznam.cz>
2024-10-28 20:46:11 +0100

    + property: createdAt

--- src/main/java/cz/godless/task_management_system/domain/Project.java
+++ src/main/java/cz/godless/task_management_system/domain/Project.java
@@ -2,10 +2,13 @@ package cz.godless.task_management_system.domain;
 
 import lombok.Value;
 
+import java.time.OffsetDateTime;
+
 @Value
 public class Project {
     long id;
     long userId;
     String name;
     String description;
+    OffsetDateTime createdAt;
 }
