16d797f8c01d3d6f4e2f68161e71da4679f57ddb
1GodLess5 <gacekalbert@seznam.cz>
2024-10-28 20:50:21 +0100

    + class: ProjectServiceJdbcImpl

---
+++ src/main/java/cz/godless/task_management_system/implementation/jdbc/service/ProjectServiceJdbcImpl.java
@@ -0,0 +1,44 @@
+package cz.godless.task_management_system.implementation.jdbc.service;
+
+import cz.godless.task_management_system.api.ProjectService;
+import cz.godless.task_management_system.api.request.ProjectAddRequest;
+import cz.godless.task_management_system.api.request.ProjectEditRequest;
+import cz.godless.task_management_system.domain.Project;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectServiceJdbcImpl implements ProjectService {
+
+    @Override
+    public Project get(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Project> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public List<Project> getAllByUser(long userId) {
+        return List.of();
+    }
+
+    @Override
+    public void delete(long id) {
+
+    }
+
+    @Override
+    public long add(ProjectAddRequest request) {
+        return 0;
+    }
+
+    @Override
+    public void edit(long id, ProjectEditRequest request) {
+
+    }
+}
