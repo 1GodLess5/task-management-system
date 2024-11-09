b5eb177f5325cf02f212567044a427b9f2f5e945
1GodLess5 <gacekalbert@seznam.cz>
2024-11-09 13:50:18 +0100

    New RestController Mapping for: update, delete

--- src/main/java/cz/godless/task_management_system/controller/ProjectController.java
+++ src/main/java/cz/godless/task_management_system/controller/ProjectController.java
@@ -2,6 +2,7 @@ package cz.godless.task_management_system.controller;
 
 import cz.godless.task_management_system.api.ProjectService;
 import cz.godless.task_management_system.api.request.ProjectAddRequest;
+import cz.godless.task_management_system.api.request.ProjectEditRequest;
 import cz.godless.task_management_system.domain.Project;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,9 +33,20 @@ public class ProjectController {
         return ResponseEntity.ok().body(projectService.get(id));
     }
 
-
     @PostMapping
     public ResponseEntity<Long> add(@RequestBody ProjectAddRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(projectService.add(request));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> update(@PathVariable("id") long id, @RequestBody ProjectEditRequest request) {
+        projectService.edit(id, request);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") long id) {
+        projectService.delete(id);
+        return ResponseEntity.ok().build();
     }
 }
