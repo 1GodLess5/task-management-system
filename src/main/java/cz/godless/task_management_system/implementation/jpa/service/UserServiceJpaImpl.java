49b6c4cf1304dba2ca1e48cf277ae6197dc092ff
1GodLess5 <gacekalbert@seznam.cz>
2024-11-30 00:25:06 +0100

    + Implementation of UserService + application.properties -> added property to disable auto modification of tables from Hibernate

--- src/main/java/cz/godless/task_management_system/implementation/jpa/service/UserServiceJpaImpl.java
+++ src/main/java/cz/godless/task_management_system/implementation/jpa/service/UserServiceJpaImpl.java
@@ -1,9 +1,18 @@
 package cz.godless.task_management_system.implementation.jpa.service;
 
 import cz.godless.task_management_system.api.UserService;
+import cz.godless.task_management_system.api.exception.BadRequestException;
+import cz.godless.task_management_system.api.exception.InternalErrorException;
+import cz.godless.task_management_system.api.exception.ResourceNotFoundException;
 import cz.godless.task_management_system.api.request.UserAddRequest;
 import cz.godless.task_management_system.domain.User;
+import cz.godless.task_management_system.implementation.jpa.entity.UserEntity;
+import cz.godless.task_management_system.implementation.jpa.repository.UserJpaRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,23 +20,53 @@ import java.util.List;
 @Service
 @Primary
 public class UserServiceJpaImpl implements UserService {
+    private final UserJpaRepository repository;
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceJpaImpl.class);
+
+    public UserServiceJpaImpl(UserJpaRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public long add(UserAddRequest request) {
-        return 0;
+        try {
+            return repository.save(new UserEntity(request.getName(), request.getEmail())).getId();
+        } catch (DataIntegrityViolationException e) {
+            throw new BadRequestException("User with email" + request.getEmail() + " already exists.");
+        } catch (DataAccessException e) {
+            logger.error("Error while adding user", e);
+            throw new InternalErrorException("Error while adding user.");
+        }
     }
 
     @Override
     public void delete(long id) {
-
+        if (this.get(id) != null) {
+            repository.deleteById(id);
+        }
     }
 
     @Override
     public User get(long id) {
-        return null;
+//        final Optional<UserEntity> userEntityOptional = repository.findById(id);
+//        if (userEntityOptional.isPresent()) {
+//            return mapUserEntityToUser(userEntityOptional.get());
+//        } else {
+//            throw new ResourceNotFoundException("User with id: " + id + " was not found.");
+//        }
+//        THE SAME CODE USING STREAM API
+        return repository.findById(id)
+                .map(this::mapUserEntityToUser)
+                .orElseThrow(() -> new ResourceNotFoundException("User with id: " + id + " was not found."));
+
     }
 
     @Override
     public List<User> getAll() {
-        return List.of();
+        return repository.findAll().stream().map(this::mapUserEntityToUser).toList();
+    }
+
+    private User mapUserEntityToUser(UserEntity entity) {
+        return new User(entity.getId(), entity.getName(), entity.getEmail());
     }
 }
