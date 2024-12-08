ce55cc722994171c4c8f643a3aa720fa1cdf6325
1GodLess5 <gacekalbert@seznam.cz>
2024-12-09 00:39:30 +0100

    + Added OpenApiDefinition annotation

--- src/main/java/cz/godless/task_management_system/TaskManagementSystemApplication.java
+++ src/main/java/cz/godless/task_management_system/TaskManagementSystemApplication.java
@@ -1,9 +1,12 @@
 package cz.godless.task_management_system;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default server URL")})
 public class TaskManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagementSystemApplication.class, args);
