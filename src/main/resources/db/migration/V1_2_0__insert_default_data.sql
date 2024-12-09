44ae05b9b3109e167a682566052c6c0af93a1fdc
1GodLess5 <gacekalbert@seznam.cz>
2024-12-09 21:11:14 +0100

    ~ Updated tables insertions for Railway

--- src/main/resources/db/migration/V1_2_0__insert_default_data.sql
+++ src/main/resources/db/migration/V1_2_0__insert_default_data.sql
@@ -1,14 +1,14 @@
 -- id, name, email
 INSERT INTO users VALUES
-                      ('Jan Novák', 'jan.novak@gmail.com'),
-                      ('Jakub Svoboda', 'jakub.svoboda@gmail.com');
+                      (1, 'Jan Novák', 'jan.novak@gmail.com'),
+                      (2, 'Jakub Svoboda', 'jakub.svoboda@gmail.com');
 -- id, user_id, name, description, created_at
 INSERT INTO project VALUES
-                        (1, 'Honzuv projekt', 'Tasky v praci', CURRENT_TIMESTAMP),
-                        (2, 'Projekt Jakuba Svobody', 'Muj Todolist', CURRENT_TIMESTAMP);
+                        (1, 1, 'Honzuv projekt', 'Tasky v praci', CURRENT_TIMESTAMP),
+                        (2, 2, 'Projekt Jakuba Svobody', 'Muj Todolist', CURRENT_TIMESTAMP);
 -- id, user_id, project_id, name, description, status, created_at
 INSERT INTO task VALUES
-                     (1, 1, 'Vytvorit API', 'API ma byt pro noveho klienta', 'DONE', CURRENT_TIMESTAMP),
-                     (1, 1, 'Otestovat API', 'Unit testy + integracni testy', 'NEW', CURRENT_TIMESTAMP),
-                     (2, 2, 'Koupit mame darek', null, 'NEW', CURRENT_TIMESTAMP),
-                     (2, null, 'Zavolat do skoly', 'Cislo mam na vizitce reditelky', 'NEW', CURRENT_TIMESTAMP);
+                     (1, 1, 1, 'Vytvorit API', 'API ma byt pro noveho klienta', 'DONE', CURRENT_TIMESTAMP),
+                     (2, 1, 1, 'Otestovat API', 'Unit testy + integracni testy', 'NEW', CURRENT_TIMESTAMP),
+                     (3, 2, 2, 'Koupit mame darek', null, 'NEW', CURRENT_TIMESTAMP),
+                     (4, 2, null, 'Zavolat do skoly', 'Cislo mam na vizitce reditelky', 'NEW', CURRENT_TIMESTAMP);
